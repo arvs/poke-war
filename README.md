166efc05ce2126debe06ec119be74bfc02f554a1
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2012-11-10 13:20:36 -0500

    cleaning up old repos

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 poke-war
 ========
-
+http://www.kudanai.com/2009/03/lets-get-poking-facebook-poke-with-bash.html
--- poke.py
+++ poke.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 
-import fbconsole as fb
-fb.authenticate()
+import mechanize
+
+class FacebookBrowser(object):
+
+	def __init__(self, email, pw):
+		self.br = mechanize.Browser()
+		self.br.set_handle_robos(False)
+		self.br.open("http://www.facebook.com/login.php")
+		self.br.select_form(predicate=lambda f: 'id' in f.attrs and f.attrs['id'] == 'login_form')
+		br['email'] = email
+		br['pass'] = pw
+		r = br.submit()
+
+	def poke_all_back(self):
+		html = self.br.open('https://www.facebook.com/pokes').get_data()
+		split_html = re.split(r'id=(\d*)\W{2}(\w*\s\w*)\<\/a\> has poked', html)
+		# group(1) per group of 3 + surrounding before & after is a[2+3i] zero-idx
+		names = [x[1] for x in filter(lambda t: (t[0] + 1) % 3 == 0, enumerate(split_html))] 
+		# group(2) per group of 3 + surrounding before & after is a[1+3i] zero-idx
+		ids = [x[1] for x in filter(lambda t: (t[0] + 2) % 3 == 0, enumerate(split_html))]
+		
